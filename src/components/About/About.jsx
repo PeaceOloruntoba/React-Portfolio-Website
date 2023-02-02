59e612e9bd7a6936e5ae30f7654ddd6e9e494c59
PeaceOloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-02-02 14:21:16 +0100

    immature

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -20,9 +20,9 @@ const About = () => {
                 <p className='pip'>As a software developer, I am passionate about using technology to solve complex problems and create innovative solutions. I have several years of experience in developing software using a variety of programming languages, including Python, PHP, Golang, and JavaScript.</p>
                 <p className='pip'>I am well-versed in various software development methodologies, including Agile and Scrum, and have experience working in both small teams and large-scale projects. I also have experience in web development, and familiar with front-end technologies like HTML, CSS, and JavaScript, as well as back-end technologies like Node.js, Flask, Django, Laravel.</p>
                 <p className='pip'>I am able to quickly learn new technologies and programming languages and am always eager to learn and grow as a developer. I am an effective communicator and enjoy working closely with other developers, designers, and stakeholders to deliver high-quality software.</p>
-                <Link href='Projects'>
+                <a href='Projects'>
                   <p className='fdsa'>Check out some of my latest projects.</p>
-                </Link>
+                </a>
             </div>
             <div className='image'>
                 <img src={aboutimg} alt="/" width='480' height='480' />
